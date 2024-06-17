8cb94a63483ff1f6e14c0758eca560bdfe1431ba
nahirm <nahir_martinez09@hotmail.com>
2024-06-17 01:46:13 -0300

    4

--- js/main.js
+++ js/main.js
@@ -1,75 +1,10 @@
-/*const serviciosDisponibles = [
-    {
-        id: 1,
-        servicio: "Esculpidas",
-        precio: "$5000",
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0HuUqFIbKDtLyIGQgtmC4JR5WiworvVpx6rLU2mJyXw&s "
-    },
-
-    {
-        id: 2,
-        servicio: "Manicuria semipermante ",
-        precio: "$3000",
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWH9WbFBzHYcelh_1W1eiD-0k_m_Uha6I0CfUHvmjk5g&s"
-    },
-
-    {
-        id: 3,
-        servicio: "Pedicuria",
-        precio: "$4000",
-        imagen: "https://previews.123rf.com/images/kremen4886/kremen48862004/kremen4886200400258/145632601-pedicura-profesional-con-bistur%C3%AD-de-dieffenbach-pod%C3%B3logo-visitante-del-paciente-procedimiento-de.jpg"
-    },
-
-    {
-        id: 4,
-        servicio: "Belleza de pies",
-        precio: "$3000",
-        imagen: "https://dcdn.mitiendanube.com/stores/446/420/products/pies-21-24fc68eec0fef885da16880435446582-1024-1024.jpeg"
-    },
-
-    {
-        id: 5,
-        servicio: "Lifting de pestañsa",
-        precio: "$5000",
-        imagen: "https://makeupmestudio.com/wp-content/uploads/2018/06/lifting-pesta.jpg"
-    },
-
-    {
-        id: 6,
-        servicio: "Extenciones de pestañas",
-        precio: "$7000",
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxm8G83E3RN1xmylm6sli4JES94P4CdtpnccbJZb8CBw&s"
-    },
-
-    {
-        id: 7,
-        servicio: "Limpieza facial",
-        precio: "$5000",
-        imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs_6KltSqAoLyB7JuL5jQDb1neFi1vp70aW2U8SkOPWw&s"
-    },
-
-]*/
-//console.log(serviciosDisponibles)
-
-/*QUEDA QUE FUNCIONE EL BOTON DE AGREGAR 
-LOCAL STORAG DE CARRITO 
-Y ONCLICK EN AGREGAR*/
-
-//const container = document.getElementById("container");
-//const btnAgregar = document.getElementById("btncarrito");
-//const buttonServicio = document.getElementById("agregar")
-//const carrito1 = document.getElementById("carrito")
-//let carrito = document.getElementById("carrito")
-//console.log(buttonServicio)
-//btnAgregar.innerText = "Agregar";
-//abtnAgregar.className = "btn-add";
 
 
 const enviar = document.getElementById("enviar");
 document.body.appendChild(enviar);
 enviar.onclick = () => Swal.fire({
     title: "Mensaje enviado!",
-    text: "Nos pondremos en contacto en las proximas 12 horas",
+    text: `Nos pondremos en contacto en las proximas 12 horas para coordinar horario`,
     icon: "success"
   });
 
@@ -164,7 +99,8 @@ function crearCard(servicio){
     const card = document.createElement("card");
     
     card.innerHTML =  `
-    <div class="divImg">
+  
+    <div class="divImg" id:"divImg">
     <img class="imagen" src="${servicio.imagen}" alt="${servicio.servicio}">
     </div>
     <div>
